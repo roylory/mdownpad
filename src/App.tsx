ebbc748bb21307af0d39312aec8b31778decb418
Royce Joe <roylory@gmail.com>
2025-04-16 17:05:30 -0700

    basic markdown conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react"
+import MarkdownIt from 'markdown-it'
+
+const md = new MarkdownIt()
+// md.use(markdownItGraph)
+
 function App() {
+  const [input, setInput] = useState("")
+
+  const [rendered, setRendered] = useState('')
+
+  useEffect(() => {
+    setRendered(md.render(input))
+  }, [input])
+
   return (
-    <h1 className="text-4xl font-semibold">mdownpad</h1>
+    <div className="flex h-screen">
+      <textarea
+        className="w-1/2 p-4 text-sm font-mono border-r resize-none focus:outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        spellCheck="false"
+      />
+      <div
+        className="w-1/2 p-4 overflow-auto prose prose-sm max-w-none"
+        dangerouslySetInnerHTML={{ __html: rendered }}
+      />
+    </div>
   )
 }
 
